33d766f753ffd528b1717da571708ab488bcc071
Adam Elfsborg <adamelfsborg.dev@gmail.com>
2021-05-20 20:45:23 +0200

    user done

--- server/user/utils.py
+++ server/user/utils.py
@@ -1,5 +1,158 @@
 from server import db
 
-class AccountType:
-    def post(self, **kwargs):
-        pass
+class Account:
+    def addTypeOfAccount(self, **kwargs):
+        sql = "INSERT INTO account_types (user_id,user_type) VALUES(%s,%s)"
+        sqldata = (kwargs['user_id'],kwargs['user_type'])
+
+        a = db.Cursor('photographyplatform', sql, sqldata)
+        account = a.connect()
+
+        if 'no results to fetch' in account:
+            return '200'
+        return '400'
+
+    def getAccountType(self, id):
+        sql = f"SELECT user_type FROM account_types WHERE user_id={id}"
+        sqldata = ()
+
+        t = db.Cursor('photographyplatform', sql, sqldata)
+        type = t.connect()
+
+        if len(type) > 0:
+            for i in type:
+                accountType = i.get('user_type')
+            return accountType
+        return '404'
+
+    def addAboutToAccount(self, **kwargs):
+        sql = "INSERT INTO abouts (user_id,about) VALUES(%s,%s)"
+        sqldata = (kwargs['user_id'],kwargs['about'])
+
+        a = db.Cursor('photographyplatform', sql, sqldata)
+        about = a.connect()
+
+        if 'no resuluts to fetch' in about:
+            return '200'
+        return '400'
+
+    def getAccountAbout(self, id):
+        sql = f"SELECT about FROM abouts WHERE user_id={id}"
+        sqldata = ()
+
+        a = db.Cursor('photographyplatform', sql, sqldata)
+        about = a.connect()
+
+        if len(about) > 0:
+            for i in about:
+                accountAbout = i.get('about')
+            return accountAbout
+        return '404'
+
+    def updateAbout(self, **kwargs):
+        sql = "UPDATE abouts SET=%s WHERE id=%s"
+        sqldata = (kwargs['about'],kwargs['id'])
+
+        a = db.Cursor('photographyplatform', sql, sqldata)
+        about = a.connect()
+
+        if 'no resuluts to fetch' in about:
+            return '200'
+        return '400'
+
+    def AddLinksToAccount(self, **kwargs):
+        sql = "INSERT INTO links (user_id,link_name,link_url) VALUES(%s,%s,%s)"
+        sqldata = (kwargs['user_id'],kwargs['link_name'],kwargs['link_urk'])
+
+        l = db.Cursor('photographyplatform', sql, sqldata)
+        link = l.connect()
+
+        if 'no resuluts to fetch' in link:
+            return '200'
+        return '400'
+    
+    def getAccountLinks(self, id):
+        sql = f"SELECT link_name, link_url FROM links WHERE user_id={id}"
+        sqldata = ()
+
+        l = db.Cursor('photographyplatform', sql, sqldata)
+        links = l.connect()
+
+        accountLinks = {
+            'links': []
+        }
+
+        if len(links) > 0:
+            for i in links:
+                accountLinks['links'].append('link_name')
+                accountLinks['links'].append('link_url')
+            return accountLinks
+        return '404'
+
+    def updateLinks(self, **kwargs):
+        sql = "UPDATE links SET=%s, SET=%s WHERE id=%s"
+        sqldata = (kwargs['link_name'],kwargs['link_url'],kwargs['id'])
+    
+    def addNoticeToAccount(self, **kwargs):
+        sql = "INSERT INTO user_notices (user_id,message) VALUES(%s,%s)"
+        sqldata = ()
+
+        n = db.Cursor('photographyplatform', sql, sqldata)
+        notice = n.connect()
+
+        if 'no resuluts to fetch' in notice:
+            return '200'
+        return '400'
+
+    def getAccountNotices(self, id):
+        sql = f"SELECT message FROM user_notices WHERE user_id={id}"
+        sqldata = ()
+
+        n = db.Cursor('photographyplatform', sql, sqldata)
+        notice = n.connect()
+
+        accountMessges = {
+            'messages': []
+        }
+
+        if len(notice) > 0:
+            for i in notice:
+                accountMessges['messages'].append('message')
+            return accountMessges
+        return '404'    
+
+    def removeNotice(self, id):
+        sql = f"DELETE FROM user_notices WHERE id={id}"
+        sqldata = ()
+
+        n = db.Cursor('photographyplatform', sql, sqldata)
+        notice = n.connect()
+
+        if 'no resuluts to fetch' in notice:
+            return '200'
+        return '400'
+
+    def rateAccount(self, **kwargs):
+        sql = "INSERT INTO photographer_ratings (user_id,photographer_id,rating) VALUES(%s,%s,%s)"
+        sqldata = (kwargs['user_id'],kwargs['photographer_id'],kwargs['rating'])
+
+        r = db.Cursor('photographyplatform', sql, sqldata)
+        rate = r.connect()
+
+        if 'no results to fetch' in rate:
+            return '200'
+        return '400'
+
+    def getAccountRating(self, id):
+        sql = f"SELECT AVG(rating) FROM photographer_ratings WHERE photographer_id={id}"
+        sqldata = ()
+
+        r = db.Cursor('photographyplatform', sql, sqldata)
+        rating = r.connect()
+
+        if len(rating) > 0:
+            for i in rating:
+                avgRating = i.get('rating')
+            return avgRating
+        return '404' 
+    
--- server/user/views.py
+++ server/user/views.py
@@ -1,3 +1,202 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from rest_framework import generics, status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from requests import Request, post, get, put
+from django.http import JsonResponse
+import ipinfo
+import socket
+import re 
+from .utils import Account
 
-# Create your views here.
+class ChooseType(APIView):
+    def post(self, request, format=None):
+        user_type = request.data.get('user_type')
+        user_id = request.data.get('user_id')
+
+        kwargs = {
+            'user_type': user_type,
+            'user_id': user_id
+        }
+        a = Account()
+        type = a.addTypeOfAccount(**kwargs)
+
+        if type != '200':
+            return JsonResponse({'msg': 'Something went wroung. Try again in 5 minutes'}, status=status.HTTP_400_BAD_REQUEST)
+        return JsonResponse({'msg': 'Success'}, status=status.HTTP_200_OK)
+
+class GetType(APIView):
+    def get(self, request, format=None):
+        user_id = request.data.get('user_id')
+
+        a = Account()
+        type = a.getAccountType(user_id)
+
+        if type == '404':
+            return JsonResponse({'msg': 'Account type not found'}, status=status.HTTP_404_NOT_FOUND)
+        return JsonResponse({'msg': type}, status=status.HTTP_200_OK)
+
+class AddAbout(APIView):
+    def post(self, request, format=None):
+        user_id = request.data.get('user_id')
+        about = request.data.get('about')
+
+        kwargs = {
+            'user_id': user_id,
+            'about': about
+        }
+
+        a = Account()
+        about = a.addAboutToAccount(**kwargs)
+
+        if about != '200':
+            return JsonResponse({'msg': 'Something went wrong. Try again in 5 minutes'}, status=status.HTTP_400_BAD_REQUEST)
+        return JsonResponse({'msg': 'About section added'}, status=status.HTTP_200_OK)
+    
+
+class GetAbout(APIView):
+    def get(self, request, format=None):
+        user_id = request.data.get('user_id')
+
+        a = Account()
+        about = a.getAccountAbout(user_id)
+
+        if about == '404':
+            return JsonResponse({'msg': 'About section not found'}, status=status.HTTP_404_NOT_FOUND)
+        return JsonResponse({'msg': about}, status=status.HTTP_200_OK)
+            
+
+class UpdateAbout(APIView):
+    def put(self, request, format=None):
+        user_id = request.data.get('user_id')
+        about = request.data.get('about')
+
+        kwargs = {
+            'user_id': user_id,
+            'about': about
+        }
+
+        a = Account()
+        update = a.updateAbout(**kwargs)
+
+        if update != '200':
+            return JsonResponse({'msg': 'Something went wrong. Try again in 5 minutes'}, status=status.HTTP_400_BAD_REQUEST)
+        return JsonResponse({'msg': 'Updated about section'}, status=status.HTTP_200_OK)
+
+class AddLinks(APIView):
+    def post(self, request, format=None):
+        user_id = request.data.get('user_id')
+        link_name = request.data.get('link_name')
+        link_url = request.data.get('link_url')
+
+        kwargs = {
+            'user_id': user_id,
+            'link_name': link_name,
+            'link_url': link_url
+        }
+
+        a = Account()
+        link = a.AddLinksToAccount(**kwargs)
+
+        if link != '200':
+            return JsonResponse({'msg': 'Something went wrong. Try again in 5 minutes'}, status=status.HTTP_400_BAD_REQUEST)
+        return JsonResponse({'msg': 'Link added'}, status=status.HTTP_200_OK)
+
+class GetLinks(APIView):
+    def get(self, request, format=None):
+        user_id = request.data.get('user_id')
+
+        a = Account()
+        links = a.getAccountLinks(user_id)
+
+        if links == '404':
+            return JsonResponse({'msg': 'Links not found'}, status=status.HTTP_404_NOT_FOUND)
+        return JsonResponse({'msg': links}, status=status.HTTP_200_OK)
+
+class UpdateLinks(APIView):
+    def put(self, request,format=None):
+        link_id = request.data.get('id')
+        link_name = request.data.get('link_name')
+        link_url = request.data.get('link_url')
+
+        kwargs = {
+            'id': link_id,
+            'link_name': link_name,
+            'link_url': link_url
+        }
+
+        a = Account()
+        link = a.updateLinks(**kwargs)
+
+        if link != '200':
+            return JsonResponse({'msg': 'Something went wrong. Try again in 5 minutes'}, status=status.HTTP_400_BAD_REQUEST)
+        return JsonResponse({'msg': 'Link updated'}, status=status.HTTP_200_OK)
+
+class AddNotice(APIView):
+    def post(self, request, format=None):
+        user_id = request.data.get('user_id')
+        message = request.data.get('message')
+
+        kwargs = {
+            'user_id': user_id,
+            'message': message
+        }
+
+        a = Account()
+        notice = a.addNoticeToAccount(**kwargs)
+
+        if notice != '200':
+            return JsonResponse({'msg': 'Something went wrong. Try again in 5 minutes'}, status=status.HTTP_400_BAD_REQUEST)
+        return JsonResponse({'msg': 'Notice Added'}, status=status.HTTP_200_OK)
+
+class GetAllNotices(APIView):
+    def get(self, request, format=None):
+        user_id = request.data.get('id')
+
+        a = Account()
+        notice = a.getAccountNotices(user_id)
+
+        if notice == '404':
+            return JsonResponse({'msg': 'Notices not found'}, status=status.HTTP_404_NOT_FOUND)
+        return JsonResponse({'msg': notice}, status=status.HTTP_200_OK)
+
+class RemoveNotice(APIView):
+    def delete(self, request, format=None):
+        id = request.data.get('id')
+
+        a = Account()
+        notice = a.removeNotice(id)
+
+        if notice != '200':
+            return JsonResponse({'msg': 'Something went wrong. Try again in 5 minutes'}, status=status.HTTP_400_BAD_REQUEST)
+        return JsonResponse({'msg': 'Notice removed'}, status=status.HTTP_200_OK)
+
+class RatePhotographer(APIView):
+    def post(self, request, format=None):
+        user_id = request.data.get('user_id')
+        photographer_id = request.data.get('photographer_id')
+        rating = request.data.get('rating')
+
+        kwargs = {
+            'user_id': user_id,
+            'photographer_id': photographer_id,
+            'rating': rating
+        }
+
+        a = Account()
+        rate = a.rateAccount(**kwargs)
+
+        if rate != '200':
+            return JsonResponse({'msg': 'Something went wrong. Try again in 5 minutes'})
+        return JsonResponse({'msg': 'Rate added'}, status=status.HTTP_200_OK)
+
+class GetPhotographerRating(APIView):
+    def get(self, request, format=None):
+        photographer_id = request.data.get('photographer')
+
+        a = Account()
+        rating = a.getAccountRating(photographer_id)
+
+        if rating == '404':
+            return JsonResponse({'msg': 'Photographer not rated'}, status=status.HTTP_404_NOT_FOUND)
+        return JsonResponse({'msg': rating}, status=status.HTTP_200_OK)
